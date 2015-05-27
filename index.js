9c0aca448b905d63df7a4823426679d07b59c618
Nicholas Eduardo <nicholasess@gmail.com>
2015-05-27 18:49:35 -0300

    remove test

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ var cheerio = require('cheerio');
 var request = require('request');
 
 module.exports = function(link, cb){
-	request(link, function(error, response, body) {
-	  var $ = cheerio.load(body);
+	if(link){
+		if(validateURL(link)){
+			request(link, function(error, response, body) {
+			  var $ = cheerio.load(body);
 
-	  var meta = $('meta')
-	  var keys = Object.keys(meta)
-	  var array = [];
+			  var meta = $('meta');
+			  var keys = Object.keys(meta);
+			  var array = [];
 
-	   keys.forEach(function(key){
-	   	if(meta[key].attribs !== undefined){
-	   		array.push(meta[key].attribs);	
-	   	}	    
-	   });
+			   keys.forEach(function(key){
+			   	if(meta[key].attribs !== undefined){
+			   		array.push(meta[key].attribs);	
+			   	}	    
+			   });
 
-	   cb(array);	   
-	});
+			   cb(array);	   
+			});
+		}else{
+			cb('Url is not valid',null);
+		}
+	}else{
+		cb('Url is empty',null);	   
+	}
+}
+
+function validateURL(textval) {
+  var urlregex = new RegExp(
+        "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
+  return urlregex.test(textval);
 }
--- test.js
+++
@@ -1,6 +0,0 @@
-var metatags = require('./app.js');
-
-var link = 'http://info.abril.com.br/noticias/internet/2015/05/outdoor-de-alimento-banido-na-russia-consegue-se-esconder-da-policia-com-reconhecimento-facial.shtml';
-metatags(link, function(data){
-console.log(data);
-});
